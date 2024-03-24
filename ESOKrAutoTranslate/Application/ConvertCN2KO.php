356be929e57f43918ec5b8ff96a5c009be32f628
DiPoon <6129876j@naver.com>
2024-03-24 16:06:58 +0900

    ConvertCN2KO 주석 추가 및 코드 정리
    힘들게 돌아갈 필요 없어서 코드를 정리합니다.

--- ESOKrAutoTranslate/Application/ConvertCN2KO.php
+++ ESOKrAutoTranslate/Application/ConvertCN2KO.php
@@ -7,36 +7,32 @@ print("\n");
 // utf-8 value 자유롭게 다루기
 $beforeString = "가나다라";
 $afterString = "";
-for ($eachChar = 0; $eachChar < mb_strlen($beforeString, "utf-8"); $eachChar++) { // beforeString 에서 한글자씩 convert
-   $slicedChar = mb_substr($beforeString, $eachChar, 1, "utf-8");
-   $hexContent = "";
-   for ($j = 0; $j < strlen($slicedChar); $j++) {
-       $splicedHexContent = dechex(ord($slicedChar[$j]));
-       $hexContent = $hexContent . $splicedHexContent;
-   }
-   $resultCharValue = hexdec("0x" . $hexContent);
+for ($i = 0; $i < mb_strlen($beforeString, "utf-8"); $i++) { // beforeString 에서 한글자씩 convert
+    // 한글자 utf-8 value
+    $eachChar = mb_substr($beforeString, $i, 1, "utf-8");
+    $beforeUTF8Value = hexdec("0x" . bin2hex($eachChar));
 
-   // 맨 마지막 $resultCharValue >= 0xE6B880 and $resultCharValue <= 0xE9A6A3 영역 빼고는 한글 -> 한자
-   if ($resultCharValue >= 0xE18480 and $resultCharValue <= 0xE187BF) {
-       $resultCharValue = $resultCharValue + 0x43400;
-   } else if ($resultCharValue > 0xE384B0 and $resultCharValue <= 0xE384BF) {
-       $resultCharValue = $resultCharValue + 0x237D0;
-   } else if ($resultCharValue > 0xE38580 and $resultCharValue <= 0xE3868F) {
-       $resultCharValue = $resultCharValue + 0x23710;
-   } else if ($resultCharValue >= 0xEAB080 and $resultCharValue <= 0xED9EAC) {
-       if ($resultCharValue >= 0xEAB880 and $resultCharValue <= 0xEABFBF) {
-           $resultCharValue = $resultCharValue - 0x33800;
-       } else if ($resultCharValue >= 0xEBB880 and $resultCharValue <= 0xEBBFBF) {
-           $resultCharValue = $resultCharValue - 0x33800;
-       } else if ($resultCharValue >= 0xECB880 and $resultCharValue <= 0xECBFBF) {
-           $resultCharValue = $resultCharValue - 0x33800;
-       } else {
-           $resultCharValue = $resultCharValue - 0x3F800;
-       }
-   } else if ($resultCharValue >= 0xE6B880 and $resultCharValue <= 0xE9A6A3) {
-       $resultCharValue = $resultCharValue + 0x3F800;
-   }
-   $afterString .= hex2bin(dechex($resultCharValue));
+     // 맨 마지막 $resultCharValue >= 0xE6B880 and $resultCharValue <= 0xE9A6A3 영역 빼고는 한글 -> 한자
+    if ($beforeUTF8Value >= 0xE18480 and $beforeUTF8Value <= 0xE187BF) {
+        $resultCharValue = $beforeUTF8Value + 0x43400;
+    } else if ($beforeUTF8Value > 0xE384B0 and $beforeUTF8Value <= 0xE384BF) {
+        $resultCharValue = $beforeUTF8Value + 0x237D0;
+    } else if ($beforeUTF8Value > 0xE38580 and $beforeUTF8Value <= 0xE3868F) {
+        $resultCharValue = $beforeUTF8Value + 0x23710;
+    } else if ($beforeUTF8Value >= 0xEAB080 and $beforeUTF8Value <= 0xED9EAC) {
+        if ($beforeUTF8Value >= 0xEAB880 and $beforeUTF8Value <= 0xEABFBF) {
+            $resultCharValue = $beforeUTF8Value - 0x33800;
+        } else if ($beforeUTF8Value >= 0xEBB880 and $beforeUTF8Value <= 0xEBBFBF) {
+            $resultCharValue = $beforeUTF8Value - 0x33800;
+        } else if ($beforeUTF8Value >= 0xECB880 and $beforeUTF8Value <= 0xECBFBF) {
+            $resultCharValue = $beforeUTF8Value - 0x33800;
+        } else {
+            $resultCharValue = $beforeUTF8Value - 0x3F800;
+        }
+    } else if ($beforeUTF8Value >= 0xE6B880 and $beforeUTF8Value <= 0xE9A6A3) {
+        $resultCharValue = $beforeUTF8Value + 0x3F800;
+    }
+    $afterString .= hex2bin(dechex($resultCharValue));
 }
 
 echo $afterString;
